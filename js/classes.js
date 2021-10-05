efc71bbe231435068a2a0edbec08072d931cbc4b
Dominika Lipka <dominika.lipka96@gmail.com>
2021-10-05 13:54:27 +0200

    improved the behaviour of moving and static obstacles

--- js/classes.js
+++ js/classes.js
@@ -7,7 +7,7 @@ class Game {
         this.snake = null;
         this.snakebody = null;
         this.item = null;
-        this.obstaclesArray = [];
+        this.staticObstaclesArray = [];
         this.movingObstaclesArray = [];
         this.currentTime = 0;
         this.score = 0;
@@ -37,51 +37,54 @@ class Game {
             this.snakebody.moveForward();
             this.snakebody.draw();
             
-            this.eatItem();
+            this.collectItemAndAddStaticObstacle();
 
 
             // HIT WALL
             if (this.snake.x + this.snake.width > 600  || this.snake.x < 0 || this.snake.y + this.snake.height > 600 || this.snake.y < 0) {
                 alert('You lost!');
-                window.location.reload();
-                // this.restartGame();
-                // this.restartScore();
-                // this.removeStaticObstacles ();
+                this.restartGame();
+                this.restartScore();
+                this.removeStaticObstacles ();
             }
 
             // HIT STATIC OBSTACLES
             this.obstaclesArray.forEach ((obstacle) => {
                 if (this.snake.x === obstacle.x && this.snake.y === obstacle.y) {
                     alert('You lost!');
-                    window.location.reload();
-                    // this.restartGame();
-                    // this.restartScore();
-                    // this.removeStaticObstacles ();
+                    this.restartGame();
+                    this.restartScore();
+                    this.removeStaticObstacles ();
                     }
             })
 
 
             // CREATE MOVING OBSTACLES
-            if (this.score > 10 && this.currentTime % 25 === 0) {
+            if (this.score > 50 && this.currentTime % 25 === 0) {
                 let newMovingObstacle = new MovingObstacle ();
                 newMovingObstacle.create();
                 this.movingObstaclesArray.push(newMovingObstacle);
             }
 
             this.movingObstaclesArray.forEach( (obstacle) => {
-                //update obstacle positions
                 obstacle.moveDown();
                 obstacle.draw();
             });
 
             // HIT MOVING OBSTACLES
             this.movingObstaclesArray.forEach ((obstacle) => {
-                if (this.snake.x === obstacle.x && this.snake.y === obstacle.y ) {
+
+                if (this.snake.x < obstacle.x + obstacle.width &&
+                    this.snake.x + obstacle.width > obstacle.x &&
+                    this.snake.y < obstacle.y + obstacle.height &&
+                    this.snake.y + this.snake.height > obstacle.y) {
                     alert('You lost!');
-                    window.location.reload();
+                    this.restartGame();
+                    this.restartScore();
+                    this.removeStaticObstacles ();
                 }
             })
-
+            
             // DELETE MOVING OBSTACLES
             this.movingObstaclesArray.forEach ((obstacle) => {
                 if (obstacle.y === 600) {
@@ -93,7 +96,7 @@ class Game {
          }, 100);
     }
 
-    eatItem () {
+    collectItemAndAddStaticObstacle () {
         if (this.snake.x === this.item.x && this.snake.y === this.item.y) {
             this.score += 10;
             scoreElm.innerHTML = this.score;
@@ -110,37 +113,29 @@ class Game {
                 let newObstacle = new StaticObstacle ();
                 newObstacle.create();
                 newObstacle.draw();
-                this.obstaclesArray.push(newObstacle);
-                console.log(this.obstaclesArray);
+                this.staticObstaclesArray.push(newObstacle);
         } 
     }
 
-    // addMovingObstacles () {
-    //     if (this.score > 200 && this.currentTime % 10 === 0) {
-    //         let newMovingObstacle = new MovingObstacle ();
-    //         newMovingObstacle.create();
-    //         newMovingObstacle.draw();
-    //     }
-    // }
 
-    // removeStaticObstacles () {
-    //     this.obstaclesArray.forEach ((obstacle) =>  {
-    //         obstacle.remove();
-    //         this.obstaclesArray.shift();
-    //     })
-    // }
+    removeStaticObstacles () {
+        this.staticObstaclesArray.forEach ((obstacle) =>  {
+            obstacle.remove();
+        })
+        this.staticObstaclesArray = [];
+    }
 
-    // restartGame () {
-    //     this.snake.x = 300;
-    //     this.snake.y = 300;
-    //     this.snakebody.x = 300;
-    //     this.snakebody.y = 300;
-    // }
+    restartGame () {
+        this.snake.x = 300;
+        this.snake.y = 300;
+        this.snakebody.x = 300;
+        this.snakebody.y = 300;
+    }
 
-    // restartScore () {
-    //     this.score = 0;
-    //     scoreElm.innerHTML = this.score;
-    // }
+    restartScore () {
+        this.score = 0;
+        scoreElm.innerHTML = this.score;
+    }
 
 
     addEventListeners () {
@@ -169,6 +164,36 @@ class Game {
                 }
                 this.snake.direction = 'Down';
                 this.snakebody.direction = 'Down';
+        }
+        })
+    }
+
+    addEventListenersNextLevel () {
+        document.addEventListener('keydown', (event) => {
+            if(event.key === 'ArrowLeft') {
+                if (this.snake.direction === 'Left') {
+                    return;
+                }
+                this.snake.direction = 'Right';
+                this.snakebody.direction = 'Right';
+        }   else if (event.key === 'ArrowRight') {
+                if (this.snake.direction === 'Right') {
+                    return;
+                }
+                this.snake.direction = 'Left';
+                this.snakebody.direction = 'Left';
+        }   else if (event.key === 'ArrowUp') {
+                if (this.snake.direction === 'Up') {
+                    return;
+                }
+                this.snake.direction = 'Down';
+                this.snakebody.direction = 'Down';
+        }   else if (event.key === 'ArrowDown') {
+             if (this.snake.direction === 'Down') {
+                    return;
+                }
+                this.snake.direction = 'Up';
+                this.snakebody.direction = 'Up';
         }
         })
     }
@@ -246,6 +271,8 @@ class Item {
         this.item = null;
         this.x = Math.floor(Math.random() * 30) * 20;
         this.y = Math.floor(Math.random() * 30) * 20;
+        this.width = 20;
+        this.height = 20;
 
     }
     create () {
@@ -255,8 +282,8 @@ class Item {
     }
 
     draw () {
-        this.item.style.width = 20 + 'px';
-        this.item.style.height = 20 + 'px';
+        this.item.style.width = this.width + 'px';
+        this.item.style.height = this.height + 'px';
         this.item.style.left = this.x + 'px';
         this.item.style.top = this.y + 'px';
     }
@@ -272,12 +299,12 @@ class StaticObstacle extends Item {
         this.item.className = 'static-obstacle'
         this.gameBoard.appendChild(this.item);
     }
+    
 }
 
 class MovingObstacle extends Item {
     constructor() {
         super();
-
         this.x = Math.floor(Math.random() * 30) * 20;
         this.y = 0;
     }
